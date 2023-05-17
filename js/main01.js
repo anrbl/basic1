9802ea641787b014e9bfac8a5bb7ddaf1c2253f1
anrbl <133613567+anrbl@users.noreply.github.com>
2023-05-17 16:42:54 +0900

    230517

---
+++ js/main01.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.mainSlide').slick({
+        /* 슬라이드 옵션 */
+        /* arrows: false, */
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+    });
+});
